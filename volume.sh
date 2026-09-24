929c9d3ed6c5c03ed97635d750f3fc97af09a9d3
MagnusNH <Magn763j@stud.ek.dk>
2026-09-24 11:25:52 +0200

    Changed

--- volume.sh
+++ volume.sh
@@ -4,6 +4,8 @@ docker volume create minvolume
 docker run --rm -v minvolume:/data alpine sh -c "echo 'Hej fra volume!' > /data/besked.txt"
 docker run --rm -v minvolume:/data alpine cat /data/besked.txt
 docker run --rm alpine cat /data/besked.txt || echo "Uden volume: filen findes ikke (som forventet)"
-docker run --rm -v minvolume:/data minside sh -c "echo 'Besked fra mit eget image!'> /data/minside-fil.txt"
+docker run -d --name minside-vol -v minvolume:/data minside
+docker exec minside-vol sh -c "echo 'Besked fra mit eget image!' > /data/minside-fil.txt"
+docker rm -f minside-vol
 docker run --rm -v minvolume:/data alpine cat /data/minside-fil.txt
 
