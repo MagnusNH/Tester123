11dd7e5058b8a407a17c168006c0e4bc98783574
MagnusNH <Magn763j@stud.ek.dk>
2026-09-24 11:18:35 +0200

    Show container wihtout volume

--- volume.sh
+++ volume.sh
@@ -3,3 +3,4 @@ set -e
 docker volume create minvolume
 docker run --rm -v minvolume:/data alpine sh -c "echo 'Hej fra volume!' > /data/besked.txt"
 docker run --rm -v minvolume:/data alpine cat /data/besked.txt
+docker run --rm alpine cat /data/besked.txt || echo "Uden volume: filen findes ikke (som forventet)"
